1b07b76ab24a3971bbc9cf632dc60d098cc5e0ac
Beksultan-08 <djumanovbeksultan@gmail.com>
2025-01-24 17:56:06 +0600

    Done homeworks

--- blog/urls.py
+++ blog/urls.py
@@ -2,16 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
     path('factorial/', views.factorial),
     path('info/', views.info),
-    path('hello/', views.hello),
-    path('sum/', views.sum),
-    path('check/', views.check),
-    path('filter/', views.filter),
-    path('palindrome/', views.palindrome),
-    path('age/', views.age),
-    path('multiplication/', views.multiplication),
-    path('max/', views.max),
-    path('temp/', views.temp)
+    path('task-1/', views.task_1),
+    path('task-7/', views.task_7),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("<p>Hello, world. You`re at the polls index.</p>")
 
 def factorial(request):
     number = int(request.GET.get['number',10])
@@ -19,58 +18,11 @@ def info(request):
         f'<p>Name:{request.GET["name"]}<br>Age:{request.GET["age"]}</p>'
     )
 
-def hello(request):
-    name = request.GET.get('name')
-    return HttpResponse(f'Hello {name}')
+def task_1(request):
+    name =  request.GET.get('name', 'Aktan')
+    return render(request, 'blog/task_1.html', {'name': name})
 
-def sum(request):
-    a = request.GET.get('a')
-    b = request.GET.get('b')
-    res = int(a) + int(b)
-    return HttpResponse(f'Result: {res}')
-
-def check(request):
-    number = request.GET.get('number')
-    a = ["Tak san" if int(number) % 2 == 1 else "Jup san"]
-    return HttpResponse(F"{number} - {a}")
-
-def filter(request):
-    text = request.GET.get('text')
-    n = int(request.GET.get('n'))
-    s = text.split()
-    d = [i for i in s if len(i) > n]
-    return HttpResponse(" ".join(d))
-
-def palindrome(request):
-    word = request.GET.get('word')
-    if word[0].lower == word[-1].lower:
-        return HttpResponse(f'{word} - palindrome')
-
-def age(request):
-    year = request.GET.get('year')
-    return HttpResponse(f'Your age - {2025 - int(year)}')
-
-def multiplication(request):
-    n = int(request.GET.get('n'))
-    return HttpResponse(
-        f"<p>{n } * { 1 } = {n * 1}<p/>"
-        f"<p>{n } * { 2 } = {n * 2}<p/>"
-        f"<p>{n } * { 3 } = {n * 3}<p/>"
-        f"<p>{n } * { 4 } = {n * 4}<p/>"
-        f"<p>{n } * { 5 } = {n * 5}<p/>"
-        f"<p>{n } * { 6 } = {n * 6}<p/>"
-        f"<p>{n } * { 7 } = {n * 7}<p/>"
-        f"<p>{n } * { 8 } = {n * 8}<p/>"
-        f"<p>{n } * { 9 } = {n * 9}<p/>"
-        f"<p>{n } * { 10 } = {n * 10}<p/>"
-    )
-
-def max(request):
-    numbers = request.GET.get('numbers')
-    a = max(numbers)
-    return HttpResponse(a)
-
-def temp(request):
-    celsius = request.GET.get('celsius')
-    fahrenheit = (celsius * 9/5) + 32
-    return HttpResponse(F"{celsius} = {fahrenheit}")
+def task_7(request):
+    number = int(request.GET.get('n', 5 ))
+    context = [f'{number} x {i} = {number * i }' for i in range(1,11)]
+    return render(request, 'blog/task_7.html',{'number': number, 'context': context})
