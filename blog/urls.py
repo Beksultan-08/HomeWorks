f5a6f31b2066227febbc6075a9c9810e91e4c704
Beksultan-08 <djumanovbeksultan@gmail.com>
2025-01-26 21:19:09 +0600

    Done all tasks

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('factorial/', views.factorial),
-    path('info/', views.info),
+    path('', views.home),
     path('task-1/', views.task_1),
+    path('task-2/', views.task_2),
+    path('task-3/', views.task_3),
+    path('task-4/', views.task_4),
+    path('task-6/', views.task_6),
     path('task-7/', views.task_7),
+    path('task-8/', views.task_8),
+    path('task-9/', views.task_9),
+    path('task-10/', views.task_10),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,58 @@
 from django.shortcuts import render
+import random
+import string
 from django.http import HttpResponse
 
 
-def factorial(request):
-    number = int(request.GET.get['number',10])
-    result = 1
-    for i in range(1, number + 1):
-        result *= i
-    return HttpResponse(f"<h1>Result: {result}</h1>")
-
-def info(request):
-    print(request.GET)
-    return HttpResponse(
-        f'<h1>Info</h1><br>'
-        f'<p>{request.build_absolute_uri()}</p>'
-        f'<p>{request.method}</p>'
-        f'<p>Name:{request.GET["name"]}<br>Age:{request.GET["age"]}</p>'
-    )
+def home(request):
+    return render(request, 'base.html')
 
 def task_1(request):
     name =  request.GET.get('name', 'Aktan')
     return render(request, 'blog/task_1.html', {'name': name})
 
+def task_2(request):
+    first = int(request.GET.get('a',5))
+    second = int(request.GET.get('b',10))
+    res = (f'{first} + {second} = {first + second}')
+    return render(request,'blog/task_2.html', {'first': first,'second': second ,'res': res})
+
+def task_3(request):
+    number = int(request.GET.get('num',7))
+    checking = [{number},"Jup" if number % 2 == 0 else "Tak"]
+    return render(request, 'blog/task_3.html', {'number': number, 'checking': checking })
+
+def task_4(request):
+    text = request.GET.get('text', 'Python Django Api Java Backend')
+    n = int(request.GET.get('n',5))
+    words = text.split()
+    filtered_words = [word for word in words if len(word) > n]
+    return render(request, 'blog/task_4.html', {'filtered_words': filtered_words})
+
+def task_6(request):
+    age = int(request.GET.get('age', 2007))
+    res = (2025 - age)
+    return render(request, 'blog/task_6.html', {'age':age, 'res': res})
+
+
 def task_7(request):
     number = int(request.GET.get('n', 5 ))
     context = [f'{number} x {i} = {number * i }' for i in range(1,11)]
     return render(request, 'blog/task_7.html',{'number': number, 'context': context})
+
+def task_8(request):
+    numbers = request.GET.get('numbers', '').split(',')
+    numbers_list = list(map(int, numbers))
+    max_number = max(numbers_list)
+    return render(request, 'blog/task_8.html', {'max_number':max_number})
+
+def task_9(request):
+    celsius = request.GET.get('celsius', 30)
+    fahrenheit = (float(celsius) * 9 / 5) + 32
+    return render(request, 'blog/task_9.html', {'fahrenheit' :fahrenheit})
+
+def task_10(request):
+    length = int(request.GET.get('length', 8))
+    characters = string.ascii_letters + string.digits
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return render(request, 'blog/task_10.html', {'password':password})
